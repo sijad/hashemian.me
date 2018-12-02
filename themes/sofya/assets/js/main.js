d72f74fc975dca7d0f14e2fa2e36791793272a3e
Sajjad Hashemian <wolaws@gmail.com>
2018-12-02 13:02:45 +0330

    init

---
+++ themes/sofya/assets/js/main.js
@@ -0,0 +1,22 @@
+var months = [
+  'فروردین',
+  'اردیبهشت',
+  'خرداد',
+  'تیر',
+  'مرداد',
+  'شهریور',
+  'مهر',
+  'آبان',
+  'آذر',
+  'دی',
+  'بهمن',
+  'اسفند',
+];
+
+document.querySelectorAll("time").forEach(function(e) {
+  var datetime = e.getAttribute("datetime").split("-").map(function(t) {
+    return parseInt(t, 10);
+  });
+  var j = toJalaali(datetime[0], datetime[1], datetime[2]);
+  e.innerText = j.jd + " " + months[j.jm] + " " + j.jy;
+});
